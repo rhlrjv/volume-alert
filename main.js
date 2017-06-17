63c3348295ed0096a9e9d5e00104a885eb78d1b7
Rahul Rajeev <rahul@rahulrajeev.co>
2017-06-17 11:59:56 +0800

    Clean up main.js

--- main.js
+++ main.js
@@ -1,103 +1,97 @@
 /*
-The MIT License (MIT)
+  The MIT License (MIT)
 
-Copyright (c) 2017 Rahul Rajeev
+  Copyright (c) 2017 Rahul Rajeev
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the 'Software'), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+  THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 */
-var audioContext = null;
-var meter = null;
-var canvasContext = null;
-var WIDTH=500;
-var HEIGHT=50;
-var rafID = null;
+let audioContext = null;
+let meter = null;
+let canvasContext = null;
+let mediaStreamSource = null;
+let rafID = null;
+
+let WIDTH = 1000;
+let HEIGHT = 200;
 
 window.onload = function() {
+  // grab our canvas
+  canvasContext = document.getElementById('meter').getContext('2d');
 
-    // grab our canvas
-	canvasContext = document.getElementById( "meter" ).getContext("2d");
+  // monkeypatch Web Audio
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-    // monkeypatch Web Audio
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  // grab an audio context
+  audioContext = new AudioContext();
 
-    // grab an audio context
-    audioContext = new AudioContext();
+  // Attempt to get audio input
+  try {
+    // monkeypatch getUserMedia
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
 
-    // Attempt to get audio input
-    try {
-        // monkeypatch getUserMedia
-        navigator.getUserMedia =
-        	navigator.getUserMedia ||
-        	navigator.webkitGetUserMedia ||
-        	navigator.mozGetUserMedia;
-
-        // ask for an audio input
-        navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream, didntGetStream);
-    } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-    }
-
-}
-
+    // ask for an audio input
+    navigator.getUserMedia(
+      {
+        'audio': {
+          'mandatory': {
+            'googEchoCancellation': 'false',
+            'googAutoGainControl': 'false',
+            'googNoiseSuppression': 'false',
+            'googHighpassFilter': 'false'
+          },
+          'optional': []
+        },
+      }, gotStream, didntGetStream);
+  } catch (e) {
+    alert('getUserMedia threw exception :' + e);
+  }
+};
 
 function didntGetStream() {
-    alert('Stream generation failed.');
-}
-
-var mediaStreamSource = null;
-
-function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-    // Create a new volume meter and connect it.
-    meter = createAudioMeter(audioContext);
-    mediaStreamSource.connect(meter);
-
-    // kick off the visual updating
-    drawLoop();
+  alert('Stream generation failed.');
 }
 
 function drawLoop( time ) {
-    // clear the background
-    canvasContext.clearRect(0,0,WIDTH,HEIGHT);
+  // clear the background
+  canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
 
-    // check if we're currently clipping
-    if (meter.checkClipping())
-        canvasContext.fillStyle = "red";
-    else
-        canvasContext.fillStyle = "green";
+  // check if we're currently clipping
+  canvasContext.fillStyle = meter.checkClipping() ? 'red' : 'green'
 
-    // draw a bar based on the current volume
-    canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
+  // draw a bar based on the current volume
+  canvasContext.fillRect(0, 0, meter.volume*WIDTH*1.4, HEIGHT);
 
-    // set up the next visual callback
-    rafID = window.requestAnimationFrame( drawLoop );
+  // set up the next visual callback
+  rafID = window.requestAnimationFrame( drawLoop );
+}
+
+function gotStream(stream) {
+  // Create an AudioNode from the stream.
+  mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+  // Create a new volume meter and connect it.
+  meter = createAudioMeter(audioContext);
+  mediaStreamSource.connect(meter);
+
+  // kick off the visual updating
+  drawLoop();
 }
